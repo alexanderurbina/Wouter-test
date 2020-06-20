3999fe1bbec5bc62a544668f55e784807d6d59f0
Alexander Urbina <rhiox117@gmail.com>
2020-06-20 15:42:36 -0400

    feature/modal desing and effects

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,16 +1,27 @@
-document.addEventListener('DOMContentLoaded', () => {
-  let img = document.getElementsByClassName("middle")[0].getElementsByTagName("img");
+document.addEventListener("DOMContentLoaded", () => {
+  let img = document
+    .getElementsByClassName("middle")[0]
+    .getElementsByTagName("img");
   for (let i = 0; i < img.length; i++) {
     const element = img[i];
-    element.addEventListener('click', () => {showModal();});
+    element.addEventListener("click", () => {
+      showModal();
+    });
   }
 });
 
 function showModal() {
-  var modalDlg = document.querySelector('#image-modal');
-  var imageModalCloseBtn = document.querySelector('#image-modal-close');
-    modalDlg.classList.add('is-active');
-    imageModalCloseBtn.addEventListener('click', function(){
-    modalDlg.classList.remove('is-active');
-});
+  var modalDlg = document.querySelector("#image-modal");
+  var imageModalCloseBtn = document.getElementsByClassName("image-modal-close");
+  var modalBackground = document.getElementsByClassName("modal-background")[0];
+  modalDlg.classList.add("is-active");
+  modalBackground.addEventListener("click", function () {
+    modalDlg.classList.remove("is-active");
+  });
+  for (let i = 0; i < imageModalCloseBtn.length; i++) {
+    const element = imageModalCloseBtn[i];
+    element.addEventListener("click", function () {
+      modalDlg.classList.remove("is-active");
+    });
+  }
 }
